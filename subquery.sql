ab81bc6da059041f48975fb71d8c708a62e902c4
olanrewaju <110352972+olanrewaju12@users.noreply.github.com>
2022-09-01 00:26:41 -0700

    Update subquery.sql

--- subquery.sql
+++ subquery.sql
@@ -1,6 +1,6 @@
-SELECT borr.[BorrowerID]
+  SELECT borr.[BorrowerID]
       ,[BORROWER NAME]	= REPLACE(CONCAT_WS(' ', borr.[BorrowerFirstName], borr.[BorrowerMiddleInitial] ,borr.[BorrowerLastName]), '  ', ' ')
-	  ,[PURCHASE AMOUNT]	=	setup.PurchaseAmount
+      ,[PURCHASE AMOUNT] = setup.PurchaseAmount
   FROM [SkyBarrelBank_UAT].[dbo].[Borrower] AS borr
   INNER JOIN [SkyBarrelBank_UAT].[dbo].[LoanSetupInformation] AS setup
   ON borr.BorrowerID = setup.BorrowerID
